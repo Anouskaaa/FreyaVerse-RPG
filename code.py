136532987bcd49958f7e424736e5ecc39e38d008
Anouskaaa <sandydcn29@gmail.com>
2026-02-08 23:01:29 +0700

    Deploy assets3

--- code.py
+++ code.py
@@ -1,11 +1,11 @@
 # 
-
-
-nickname = ["Dodo_Gergaji", "Edo_Knalpot", "Mail_Sepeda", "Supri_Boring", "Asep_Stroberi"]
-password = []
-level = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-inventory = []
-
+print("-"*10, " Welcome to FreyaVerse RPG ", "-"*10)
+#
+# nickname = ["Dodo_Gergaji", "Edo_Knalpot", "Mail_Sepeda", "Supri_Boring", "Asep_Stroberi"]
+# password = []
+# level = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# inventory = []
+#
 
 
 def input_data():
