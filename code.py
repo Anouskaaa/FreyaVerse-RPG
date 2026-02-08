e4106b58bf26c2466480be8850c20f6e43ff93de
Anouskaaa <sandydcn29@gmail.com>
2026-02-08 22:50:16 +0700

    Deploy assets

---
+++ code.py
@@ -0,0 +1,30 @@
+# 
+
+
+nickname = ["Dodo_Gergaji", "Edo_Knalpot", "Mail_Sepeda", "Supri_Boring", "Asep_Stroberi"]
+password = []
+level = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+inventory = []
+
+
+
+def input_data():
+
+    pass
+
+def tampil_data():
+    
+
+    pass
+
+def pengurutan_data():
+    
+
+    pass
+
+def pencarian_data():
+
+
+    pass
+
+
